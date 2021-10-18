d473e47cbe73e67a29a9841f9cb180db12e1be2e
manoj suryawanshi <er.manojsuryawanshi2020@gmail.com>
2021-10-18 12:13:03 +0530

    testone

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,24 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import static org.testng.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+@Test
 class StringCalculatorShould {
+	
+	private StringCalculator stringCalculator;
+	
+	@BeforeTest
+	public void init() {
+		stringCalculator = new StringCalculator();
+	}
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+   
+   public void empty_string_should_return_0() throws Exception {
+        assertEquals(stringCalculator.add(""),0);   
     }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+ 
 }
